b31ec32709e671317d8832132b4a55148d5045f6
Mikus Rakis <mikus.rakis@gmail.com>
2026-07-03 13:57:07 +0300

    created sql queries

--- src/main/resources/insert.sql
+++ src/main/resources/insert.sql
@@ -1,21 +1,19 @@
 PRAGMA foreign_keys = ON;
 
 -- Delete students first because students depends on courses.
-DELETE FROM students;
-DELETE FROM courses;
+DELETE
+FROM students;
+DELETE
+FROM courses;
 
--- TODO 1:
--- Insert 3 courses.
--- Remember:
--- course_id is a number.
--- course_name must be present.
--- credits must be greater than 0.
+INSERT INTO courses(course_id, course_name, credits)
+VALUES (1, 'Intro to Java', 2),
+       (2, 'Java', 4),
+       (3, 'Outro to Java', 8);
 
--- TODO 2:
--- Insert 5 students.
--- Remember:
--- id is a number.
--- name must be present.
--- email must be unique.
--- age must be 18 or older.
--- course_id must exist in the courses table.
+INSERT INTO students (id, name, email, age, course_id)
+VALUES (1, 'Peter', 'pete@gmail.com', 20, 1),
+       (2,'John','joe@gmail.com',23,1),
+       (3,'James','james@gmail.com',32,2),
+       (4,'Maia','maie@jupiter.org',19,3),
+       (5,'Dione','dione@saturn.info',49,1);
--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -1,32 +1,46 @@
 PRAGMA foreign_keys = ON;
 
--- TODO 1:
--- Select all courses.
+SELECT *
+FROM courses;
 
--- TODO 2:
--- Select all students.
+SELECT *
+FROM students;
 
--- TODO 3:
--- Select students older than 20.
+SELECT *
+FROM students
+WHERE age > 20;
 
--- TODO 4:
--- Show student names together with course names.
+SELECT students.name, courses.course_name
+FROM students
+         JOIN courses ON students.course_id = courses.course_id
 
--- TODO 5:
--- Count how many students are in each course.
+SELECT students.course_id,
+       course_name,
+       COUNT(*)
+FROM students
+         INNER JOIN courses ON
+    students.course_id = courses.course_id
+GROUP BY students.course_id;
 
--- TODO 6:
--- Update the first students age.
+UPDATE students
+SET age = 87
+WHERE id = 1;
 
--- Check the update.
+SELECT name, age
+FROM students
+WHERE id = 1;
 
--- TODO 7:
--- Move second student to a different course.
+UPDATE students
+SET course_id = 3
+WHERE id = 2;
 
--- Check the update using JOIN.
+SELECT students.name, courses.course_name
+FROM students
+         JOIN courses ON students.course_id = courses.course_id
+WHERE id = 2;
 
--- TODO 8:
--- Delete one student.
+DELETE FROM students
+WHERE id = 3;
 
--- TODO 9:
--- Check the final result.
+SELECT *
+FROM students;
