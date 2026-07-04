f9c44b5b27383ff7f852ff6774832ed5f91c1221
Mikus Rakis <mikus.rakis@gmail.com>
2026-07-04 23:45:20 +0300

    add missing null checks, move constraints to field

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -9,8 +9,7 @@ CREATE TABLE courses
 (
     course_id   INTEGER PRIMARY KEY,
     course_name TEXT NOT NULL,
-    credits     INTEGER,
-    CHECK ( credits > 0 )
+    credits     INTEGER NOT NULL CHECK ( credits > 0 )
 );
 
 CREATE TABLE students
@@ -18,9 +17,7 @@ CREATE TABLE students
     id        INTEGER PRIMARY KEY,
     name      TEXT    NOT NULL,
     email     TEXT    NOT NULL UNIQUE,
-    age       INTEGER,
+    age       INTEGER NOT NULL CHECK ( age >= 18 ),
     course_id INTEGER NOT NULL,
-    FOREIGN KEY (course_id) REFERENCES courses (course_id),
-
-    CHECK ( age >= 18 )
+    FOREIGN KEY (course_id) REFERENCES courses (course_id)
 );
